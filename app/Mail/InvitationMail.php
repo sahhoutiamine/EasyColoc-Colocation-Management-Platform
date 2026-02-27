95ed75b2bc8f62700c03e2ef8c233d0850307ea6
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-02-27 11:46:29 +0100

    feat: create invitation email mailable class for sending colocation invites

---
+++ app/Mail/InvitationMail.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class InvitationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $invitation;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(\App\Models\Invitation $invitation)
+    {
+        $this->invitation = $invitation;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Join our colocation: ' . $this->invitation->colocation->name,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.invitation',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
